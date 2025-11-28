edac0573d58b4c657ff1e5398c0f60eb462d7bb4
Sugiyah <sugiyah06121958@gmail.com>
2025-11-28 03:00:14 +0000

    setelah sya publish di github kok muncul seperti ini ya

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/studio',
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -9,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',

03df8e5395db4dedb9c59b6c93a6301408de88c9
Sugiyah <sugiyah06121958@gmail.com>
2025-11-28 03:36:16 +0000

    masih tetap tidak bisa tolong perbaiki ya

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: '/studio',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
